fe87f1ee99d9bc962a0509c481d653639d0312c0
fede-berga <jackes779@gmail.com>
2021-03-08 11:25:47 +0100

    Comments, method names and struct fiels in english

--- src/maf_paser.rs
+++ src/maf_paser.rs
@@ -5,17 +5,16 @@ use multiple_alignment_format::MAFBlock;
 use multiple_alignment_format::parser::next_maf_item;
 use std::fmt;
 
-/// Rappresenta un blocco di allineamento 
-/// in un file .maf
+/// Represents al alignment block in a MAF file 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Alignment(pub Vec<Sequence>);
 
-/// Rappresenta una sequenza di allineamento.
+/// Represents an aligned sequence
 #[derive(Debug, PartialEq, Eq)]
 pub struct Sequence {
-    /// Nome della sequenza
-    pub nome : String,
-    /// Sequenza allineata
+    /// Sequence name
+    pub name : String,
+    /// Aligned Sequence
     pub seq : Vec<u8>,
 }
 
@@ -31,7 +30,7 @@ impl fmt::Display for Alignment {
 
 impl fmt::Display for Sequence {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} : [", self.nome)?;
+        write!(f, "{} : [", self.name)?;
         for ch in self.seq.iter() {
             write!(f, "{}", *ch as char)?;
             if ch != self.seq.iter().last().unwrap() {
@@ -43,14 +42,14 @@ impl fmt::Display for Sequence {
 }
 
 impl Alignment {
-    /// Restituisce un Alignment contenuto nel file 'file_name'
+    /// Return the first alignment contained in 'file_name'
     /// 
     /// # Arguments
     /// 
-    /// * 'file_name' - Nome del file da cui prelevare l'allineamento
+    /// * 'file_name' - File from which the alignment block is fetched
     pub fn new(file_name : &str) -> Result<Alignment, &'static str> {
         let contents = match Alignment::get_file_content(file_name) {
-            Err(_) => return Err("Errore nella lettura del file"),
+            Err(_) => return Err("Error in file reading"),
             Ok(result) => result,
         };
     
@@ -61,21 +60,21 @@ impl Alignment {
         Ok(Alignment::get_alignments(alignment_block))
     }
 
-    /// Restituisce il contenuto del file 'file_name'
+    /// Returns the content of 'file_name'
     /// 
     /// # Arguments
     /// 
-    /// * 'file_name' - Nome del file da cui prelevare il contenuto
+    /// * 'file_name' - File from which the cntent is fetched
     fn get_file_content(file_name : & str) -> io::Result<String> {
         fs::read_to_string(file_name)
     }
     
-    /// Restituisce il primo blocco di allineamento nel file .maf.
-    /// Restituisce un errore nel caso non venga trovato nessun blocco.
+    /// Returns the first alignment block contained in 'maf_contents'.
+    /// Returns an error if there is no alignment block in 'maf_contents'.
     /// 
     /// # Arguments
     /// 
-    /// * 'maf_contents' - Contenuto del file .maf
+    /// * 'maf_contents' - Content of a MAf file
     fn get_block(maf_contents : String) -> Result<MAFBlock, &'static str> {
         for line in maf_contents.lines() {
             let i = maf_contents.find(line).unwrap();
@@ -86,20 +85,20 @@ impl Alignment {
             }
         }
         
-        Err("Blocco di allineamento non trovato")
+        Err("Alignment block not found")
     }
     
-    /// Restituisce un Alignment contentente i nomi delle sequenze e la relativa
-    /// sequenza allineata.
+    /// Returns an Alignment containing, for each sequence, 
+    /// the name and the relative aligned sequence
     /// 
     ///  # Arguments
     /// 
-    /// * 'block' - Blocco contenente l'allineamento
+    /// * 'block' - Block containing the alignment
     fn get_alignments(block : MAFBlock) -> Alignment{
         let alignment = block.aligned_entries()
             .map(|aligned_entry| {
                 Sequence {
-                    nome : aligned_entry.seq.clone(),
+                    name : aligned_entry.seq.clone(),
                     seq : aligned_entry.alignment.clone(),
                 }
             })
--- tests/parser_test.rs
+++ tests/parser_test.rs
@@ -4,23 +4,23 @@ use variation_graph::{
 };
 
 #[test]
-fn parse_file_non_presente() {
-    let al = Alignment::new("./dataset/non_presente.maf");
-    let expected : Result<Alignment, &str> = Err("Errore nella lettura del file");
+fn parse_file_not_found() {
+    let al = Alignment::new("./dataset/file_not_found.maf");
+    let expected : Result<Alignment, &str> = Err("Error in file reading");
     assert_eq!(al, expected);
 }
 
 #[test]
 fn parse_emply_file() {
     let al = Alignment::new("./dataset/empty.maf");
-    let expected : Result<Alignment, &str> = Err("Blocco di allineamento non trovato");
+    let expected : Result<Alignment, &str> = Err("Alignment block not found");
     assert_eq!(al, expected);
 }
 
 #[test]
 fn parse_file_no_alignment_block() {
     let al = Alignment::new("./dataset/no_alignment_block.maf");
-    let expected : Result<Alignment, &str> = Err("Blocco di allineamento non trovato");
+    let expected : Result<Alignment, &str> = Err("Alignment block not found");
     assert_eq!(al, expected);
 }
 
@@ -30,23 +30,23 @@ fn parse_file_one_alignment_block() {
     let expected : Vec<Sequence> = 
     vec![
         Sequence {
-            nome : String::from("hg18.chr7"),
+            name : String::from("hg18.chr7"),
             seq : vec![65, 65, 65, 45, 71, 71, 71, 65, 65, 84, 71, 84, 84, 65, 65, 67, 67, 65, 65, 65, 84, 71, 65, 45, 45, 45, 65, 84, 84, 71, 84, 67, 84, 67, 84, 84, 65, 67, 71, 71, 84, 71],
         },
         Sequence{
-            nome : String::from("panTro1.chr6"),
+            name : String::from("panTro1.chr6"),
             seq : vec![65, 65, 65, 45, 71, 71, 71, 65, 65, 84, 71, 84, 84, 65, 65, 67, 67, 65, 65, 65, 84, 71, 65, 45, 45, 45, 65, 84, 84, 71, 84, 67, 84, 67, 84, 84, 65, 67, 71, 71, 84, 71],
         },
         Sequence {
-            nome : String::from("baboon"),
+            name : String::from("baboon"),
             seq :  vec![65, 65, 65, 45, 71, 71, 71, 65, 65, 84, 71, 84, 84, 65, 65, 67, 67, 65, 65, 65, 84, 71, 65, 45, 45, 45, 71, 84, 84, 71, 84, 67, 84, 67, 84, 84, 65, 84, 71, 71, 84, 71],
         },
         Sequence {
-            nome : String::from("mm4.chr6"),
+            name : String::from("mm4.chr6"),
             seq : vec![45, 65, 65, 84, 71, 71, 71, 65, 65, 84, 71, 84, 84, 65, 65, 71, 67, 65, 65, 65, 67, 71, 65, 45, 45, 45, 65, 84, 84, 71, 84, 67, 84, 67, 84, 67, 65, 71, 84, 71, 84, 71],
         },
         Sequence {
-            nome :  String::from("rn3.chr4"),
+            name :  String::from("rn3.chr4"),
             seq : vec![45, 65, 65, 45, 71, 71, 71, 71, 65, 84, 71, 67, 84, 65, 65, 71, 67, 67, 65, 65, 84, 71, 65, 71, 84, 84, 71, 84, 84, 71, 84, 67, 84, 67, 84, 67, 65, 65, 84, 71, 84, 71],
         }
     ];
@@ -54,20 +54,20 @@ fn parse_file_one_alignment_block() {
 }
 
 #[test]
-fn parse_file_one_alignment_block_with_info_line() {
+fn parse_file_one_alignment_block_with_info_lines() {
     let al = Alignment::new("./dataset/one_al_block_with_info.maf").unwrap();
     let expected : Vec<Sequence> = 
     vec![
         Sequence {
-            nome : String::from("hg16.chr7"),
+            name : String::from("hg16.chr7"),
             seq : vec![103, 99, 97, 103, 99, 116, 103, 97, 97, 97, 97, 99, 97],
         },
         Sequence{
-            nome : String::from("panTro1.chr6"),
+            name : String::from("panTro1.chr6"),
             seq : vec![103, 99, 97, 103, 99, 116, 103, 97, 97, 97, 97, 99, 97],
         },
         Sequence {
-            nome : String::from("baboon"),
+            name : String::from("baboon"),
             seq :  vec![103, 99, 97, 103, 99, 116, 103, 97, 97, 97, 97, 99, 97],
         }
     ];
@@ -80,23 +80,23 @@ fn parse_file_mul_alignment() {
     let expected : Vec<Sequence> = 
     vec![
         Sequence {
-            nome : String::from("hg18.chr7"),
+            name : String::from("hg18.chr7"),
             seq : vec![65, 65, 65, 45, 71, 71, 71, 65, 65, 84, 71, 84, 84, 65, 65, 67, 67, 65, 65, 65, 84, 71, 65, 45, 45, 45, 65, 84, 84, 71, 84, 67, 84, 67, 84, 84, 65, 67, 71, 71, 84, 71],
         },
         Sequence{
-            nome : String::from("panTro1.chr6"),
+            name : String::from("panTro1.chr6"),
             seq : vec![65, 65, 65, 45, 71, 71, 71, 65, 65, 84, 71, 84, 84, 65, 65, 67, 67, 65, 65, 65, 84, 71, 65, 45, 45, 45, 65, 84, 84, 71, 84, 67, 84, 67, 84, 84, 65, 67, 71, 71, 84, 71],
         },
         Sequence {
-            nome : String::from("baboon"),
+            name : String::from("baboon"),
             seq :  vec![65, 65, 65, 45, 71, 71, 71, 65, 65, 84, 71, 84, 84, 65, 65, 67, 67, 65, 65, 65, 84, 71, 65, 45, 45, 45, 71, 84, 84, 71, 84, 67, 84, 67, 84, 84, 65, 84, 71, 71, 84, 71],
         },
         Sequence {
-            nome : String::from("mm4.chr6"),
+            name : String::from("mm4.chr6"),
             seq : vec![45, 65, 65, 84, 71, 71, 71, 65, 65, 84, 71, 84, 84, 65, 65, 71, 67, 65, 65, 65, 67, 71, 65, 45, 45, 45, 65, 84, 84, 71, 84, 67, 84, 67, 84, 67, 65, 71, 84, 71, 84, 71],
         },
         Sequence {
-            nome :  String::from("rn3.chr4"),
+            name :  String::from("rn3.chr4"),
             seq : vec![45, 65, 65, 45, 71, 71, 71, 71, 65, 84, 71, 67, 84, 65, 65, 71, 67, 67, 65, 65, 84, 71, 65, 71, 84, 84, 71, 84, 84, 71, 84, 67, 84, 67, 84, 67, 65, 65, 84, 71, 84, 71],
         }
     ];
