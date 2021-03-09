76892bc536f32024720e9d1b674e4eb94e33114d
fede-berga <jackes779@gmail.com>
2021-03-09 18:31:05 +0100

    Second possible solution

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use variation_graph::{
     maf_paser::{Alignment, Sequence},
-    variation_graph:: {build_vg_from_seq_alignment},
+    variation_graph::{
+        build_vg_V1,
+        build_vg_v2,
+    },
 };
 /*use handlegraph::{
     handle::{Direction, Edge, Handle, NodeId},
@@ -24,7 +27,7 @@ fn main() {
             name : String::from("panTro1.chr6"),
             seq : vec![65, 65, 65, 45, 71, 71, 71, 65, 65, 84, 71, 84, 84, 65, 65, 67, 67, 65, 65, 65, 84, 71, 65, 45, 45, 45, 65, 84, 84, 71, 84, 67, 84, 67, 84, 84, 65, 67, 71, 71, 84, 71],
         },
-        Sequence {
+        /*Sequence {
             name : String::from("baboon"),
             seq :  vec![65, 65, 65, 45, 71, 71, 71, 65, 65, 84, 71, 84, 84, 65, 65, 67, 67, 65, 65, 65, 84, 71, 65, 45, 45, 45, 71, 84, 84, 71, 84, 67, 84, 67, 84, 84, 65, 84, 71, 71, 84, 71],
         },
@@ -34,10 +37,11 @@ fn main() {
         },
         Sequence {
             name :  String::from("rn3.chr4"),
-            seq : vec![45, 66, 65, 45, 71, 71, 71, 71, 65, 84, 71, 67, 84, 65, 65, 71, 67, 67, 65, 65, 84, 71, 65, 71, 84, 84, 71, 84, 84, 71, 84, 67, 84, 67, 84, 67, 65, 65, 84, 71, 84, 71],
-        }
+            seq : vec![45, 65, 65, 45, 71, 71, 71, 71, 65, 84, 71, 67, 84, 65, 65, 71, 67, 67, 65, 65, 84, 71, 65, 71, 84, 84, 71, 84, 84, 71, 84, 67, 84, 67, 84, 67, 65, 65, 84, 71, 84, 71],
+        }*/
     ];
 
     let expected = Alignment(al);
-    build_vg_from_seq_alignment(&expected);
+    build_vg_v2(&expected);
+    println!("allignment occ mem : {}", std::mem::size_of_val(&expected));
 }
--- src/variation_graph.rs
+++ src/variation_graph.rs
@@ -16,7 +16,51 @@ use handlegraph::{
 
 const INDEL : u8 = '-' as u8;
 
-pub fn build_vg_from_seq_alignment(alignment : &Alignment) -> () {
+pub fn build_vg_v2(alignment : &Alignment) ->() {
+    let mut vg = HashGraph::new();
+    let mut path = Vec::new();
+    let first_handle = vg.append_handle(b"First_node");
+    let mut prev_handle = vec![first_handle; alignment.0.len()];
+
+    for seq in alignment.0.iter() {
+        let p = vg.create_path(seq.name.as_bytes(), false).unwrap();
+        vg.path_append_step(p, first_handle);
+        path.push(p);
+    }
+
+    println!("{:?}", prev_handle);
+
+    for i in 0..alignment.0[0].seq.len() {
+        let mut current_nucleotide = Vec::new();
+
+        for sequence in alignment.0.iter() {
+            if sequence.seq[i] != INDEL && !current_nucleotide.contains(&sequence.seq[i]) {
+                current_nucleotide.push(sequence.seq[i]);
+            }
+        }
+
+        println!("{:?}", current_nucleotide);
+
+        for nucleotide in current_nucleotide {
+            let handle = vg.append_handle(&vec![nucleotide]);
+            for (j, sequence) in alignment.0.iter().enumerate() {
+                if sequence.seq[i] == nucleotide {
+                    vg.create_edge(Edge(prev_handle[j], handle));
+                    prev_handle[j] = handle;
+                    vg.path_append_step(path[j], handle);
+                }
+            }
+        }
+    }
+
+    for path_item in path.iter() {
+        vg.print_path(path_item);
+    }
+
+    println!("graph occ mem : {}", std::mem::size_of_val(&vg));
+}
+
+pub fn build_vg_V1(alignment : &Alignment) -> () {
     let mut current_index : Vec<usize> = vec![0; alignment.0.len()];
     let (mut vg, path) = initialize_graph(&alignment);
 
@@ -52,8 +96,6 @@ pub fn build_vg_from_seq_alignment(alignment : &Alignment) -> () {
     for path_item in path.iter() {
         vg.print_path(path_item);
     }
-
-    println!("{:?}", vg);
 }
 
 fn forward_until_not_hyphen(seq : &[u8], current : usize) -> Option<usize>{
