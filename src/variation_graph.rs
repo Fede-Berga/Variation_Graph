78858279424bccb8e7af697ee4d7cdf034b1e867
fede-berga <jackes779@gmail.com>
2021-03-09 16:43:15 +0100

    Possible solution

--- src/variation_graph.rs
+++ src/variation_graph.rs
@@ -1,44 +1,61 @@
-use crate::maf_paser::Alignment;
-/*use handlegraph::{
+use crate::maf_paser::{
+    Alignment,
+    //Sequence,
+};
+use handlegraph::{
     handle::{Direction, Edge, Handle, NodeId},
     handlegraph::*,
     hashgraph::{
         path::{Step, StepIx},
         HashGraph,
+        Node,
     },
     mutablehandlegraph::*,
     pathhandlegraph::*,
-};*/
+};
 
-const NUCLOTIDES : &[u8]= ['A' as u8, 'C' as u8, 'G' as u8,'T' as u8];
 const INDEL : u8 = '-' as u8;
 
 pub fn build_vg_from_seq_alignment(alignment : &Alignment) -> () {
-    let mut current_index : Vec<usize> = vec![23; alignment.0.len()];
-    //let mut vg = HashGraph::new();
-
-    println!("prima stampa{:?}", current_index);
-    println!("len : {}", alignment.0[0].seq.len());
+    let mut current_index : Vec<usize> = vec![0; alignment.0.len()];
+    let (mut vg, path) = initialize_graph(&alignment);
+    
+    //println!("Sequence length : {}", alignment.0[0].seq.len());
 
     while current_index.iter().enumerate().any(|(i, &current)| current < alignment.0[i].seq.len()) {
-        current_index = current_index
-            .iter()
-            .enumerate()
-            .map(|(i, &current)| {
-                match alignment.0[i].seq[current] {
-                    INDEL => match forward_until_not_hyphen(&alignment.0[i].seq, current) {
-                        Some(pos) => pos,
-                        _ => alignment.0[i].seq.len(),
-                    },
-                    _ => current,
-                }
-            })
-            .collect();
+        current_index = set_current_index(&alignment, current_index);
         
-        println!("{:?}", current_index);
+        println!("current_index : {:?}", current_index);
 
         //Build Graph
+
+        let mut current_nucleotide = Vec::new();
+
+        for (i, &current) in current_index.iter().enumerate() {
+            if current < alignment.0[i].seq.len() && !current_nucleotide.contains(&alignment.0[i].seq[current]) {
+                current_nucleotide.push(alignment.0[i].seq[current]);
+            }
+        }
+
+        println!("{:?}", current_nucleotide);
+
+        for nucleotide in current_nucleotide {
+            let handle = vg.append_handle(&vec![nucleotide]);
+            for (i, &current) in current_index.iter().enumerate() {
+                if current < alignment.0[i].seq.len() && alignment.0[i].seq[current] == nucleotide {
+                    vg.path_append_step(path[i], handle);
+                }
+            } 
+        }
+        
+        current_index = current_index.iter().map(|&current| current + 1).collect();
     }
+
+    for path_item in path.iter() {
+        vg.print_path(path_item);
+    }
+
+    println!("{:?}", vg);
 }
 
 fn forward_until_not_hyphen(seq : &[u8], current : usize) -> Option<usize>{
@@ -46,4 +63,37 @@ fn forward_until_not_hyphen(seq : &[u8], current : usize) -> Option<usize>{
         Some(pos) => Some(pos + current),
         _ => None,
     }
+}
+
+fn initialize_graph(alignment : &Alignment) -> (HashGraph, Vec<PathId>) {
+    let mut graph = HashGraph::new();
+    let mut path = Vec::new();
+    let first_handle = graph.append_handle(b"First_node");
+
+    for seq in alignment.0.iter() {
+        let p = graph.create_path(seq.name.as_bytes(), false).unwrap();
+        graph.path_append_step(p, first_handle);
+        path.push(p);
+    }
+
+    (graph, path)
+}
+
+fn set_current_index(alignment : &Alignment, current_index : Vec<usize>) -> Vec<usize> {
+    current_index.iter()
+            .enumerate()
+            .map(|(i, &current)| {
+                if current >= alignment.0[i].seq.len() {
+                    current
+                } else {
+                    match alignment.0[i].seq[current] {
+                        INDEL => match forward_until_not_hyphen(&alignment.0[i].seq, current) {
+                            Some(pos) => pos,
+                            _ => alignment.0[i].seq.len(),
+                        },
+                        _ => current,
+                    }
+                }
+            })
+            .collect()
 }
