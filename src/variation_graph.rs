7d72c043b1656e32d34dee6552a79e4de637c994
fede-berga <jackes779@gmail.com>
2021-03-11 11:35:43 +0100

    Making Tests

--- src/variation_graph.rs
+++ src/variation_graph.rs
@@ -18,25 +18,24 @@ pub struct VariationGraph {
 }
 
 impl VariationGraph {
-    pub fn new(file_name : &str) -> Result<VariationGraph, &'static str> {
-        match Alignment::new(file_name) {
-            Ok(alignment) => Ok(VariationGraph::build_graph(&alignment)),
-            Err(error) => Err(error),
-        }
+    pub fn new(alignment : &Alignment) -> VariationGraph {
+        let vg = VariationGraph::build_vg(&alignment);
+        VariationGraph {graph : vg}
     }
 
-    pub fn build_graph(alignment : &Alignment) -> VariationGraph {
-        let vg = VariationGraph::build_vg(&alignment);
-        VariationGraph {graph : vg,}
+    pub fn print_path(&self, path_name : &[u8]) -> Result<(), String> {
+        match self.graph.path_id.get(path_name) {
+            Some(path) => {
+                println!("Path name : {}", std::str::from_utf8(path_name).unwrap());
+                self.graph.print_path(path);
+                Ok(())
+            },
+            _ => Err(format!("Path \'{}\' does not exist", std::str::from_utf8(path_name).unwrap())),
+        }
     }
 
     fn build_vg(alignment : &Alignment) -> HashGraph {
         let (mut vg, path, mut prev_handle) = VariationGraph::init(&alignment);
-    
-        //println!("After Initialization : ");
-        //println!("vg : {:#?}", vg);
-        //println!("path : {:#?}", path);
-        //println!("prev_handle : {:#?}", prev_handle);
     
         for i in 0..alignment.0[0].seq.len() {
             let mut current_nucleotide = Vec::new();
@@ -46,8 +45,6 @@ impl VariationGraph {
                     current_nucleotide.push(sequence.seq[i]);
                 }
             }
-    
-            println!("{:?}", current_nucleotide);
     
             for nucleotide in current_nucleotide {
                 let handle = vg.append_handle(&vec![nucleotide]);
@@ -60,7 +57,14 @@ impl VariationGraph {
                 }
             }
         }
-        //Add Last Node
+        
+        let last_handle = vg.append_handle(b"Last_node");
+
+        for (i, handle) in prev_handle.into_iter().enumerate() {
+            vg.create_edge(Edge(handle, last_handle));
+            vg.path_append_step(path[i], last_handle);
+        }
+        
         vg
     }
 
@@ -79,82 +83,3 @@ impl VariationGraph {
         (vg, path, prev_handle)
     }
 }
-/*
-pub fn build_vg_V1(alignment : &Alignment) -> () {
-    let mut current_index : Vec<usize> = vec![0; alignment.0.len()];
-    let (mut vg, path) = initialize_graph(&alignment);
-
-    while current_index.iter().enumerate().any(|(i, &current)| current < alignment.0[i].seq.len()) {
-        current_index = set_current_index(&alignment, current_index);
-        
-        println!("current_index : {:?}", current_index);
-
-        //Build Graph
-
-        let mut current_nucleotide = Vec::new();
-
-        for (i, &current) in current_index.iter().enumerate() {
-            if current < alignment.0[i].seq.len() && !current_nucleotide.contains(&alignment.0[i].seq[current]) {
-                current_nucleotide.push(alignment.0[i].seq[current]);
-            }
-        }
-
-        println!("{:?}", current_nucleotide);
-
-        for nucleotide in current_nucleotide {
-            let handle = vg.append_handle(&vec![nucleotide]);
-            for (i, &current) in current_index.iter().enumerate() {
-                if current < alignment.0[i].seq.len() && alignment.0[i].seq[current] == nucleotide {
-                    vg.path_append_step(path[i], handle);
-                }
-            } 
-        }
-        
-        current_index = current_index.iter().map(|&current| current + 1).collect();
-    }
-
-    for path_item in path.iter() {
-        vg.print_path(path_item);
-    }
-}
-
-fn forward_until_not_hyphen(seq : &[u8], current : usize) -> Option<usize>{
-    match seq[current..].iter().position(|&ch| ch != INDEL) {
-        Some(pos) => Some(pos + current),
-        _ => None,
-    }
-}
-
-fn initialize_graph(alignment : &Alignment) -> (HashGraph, Vec<PathId>) {
-    let mut graph = HashGraph::new();
-    let mut path = Vec::new();
-    let first_handle = graph.append_handle(b"First_node");
-
-    for seq in alignment.0.iter() {
-        let p = graph.create_path(seq.name.as_bytes(), false).unwrap();
-        graph.path_append_step(p, first_handle);
-        path.push(p);
-    }
-
-    (graph, path)
-}
-
-fn set_current_index(alignment : &Alignment, current_index : Vec<usize>) -> Vec<usize> {
-    current_index.iter()
-            .enumerate()
-            .map(|(i, &current)| {
-                if current >= alignment.0[i].seq.len() {
-                    current
-                } else {
-                    match alignment.0[i].seq[current] {
-                        INDEL => match forward_until_not_hyphen(&alignment.0[i].seq, current) {
-                            Some(pos) => pos,
-                            _ => alignment.0[i].seq.len(),
-                        },
-                        _ => current,
-                    }
-                }
-            })
-            .collect()
-}
-*/
--- tests/variation_graph_test.rs
+++ tests/variation_graph_test.rs
@@ -14,15 +14,29 @@ use variation_graph::{
     pathhandlegraph::*,
 };*/
 
-#[test]
-fn file_not_found() {
-    let graph = VariationGraph::new("./dataset/file_not_found.maf");
-    assert_eq!("Error in file reading", graph.err().unwrap());
+fn run_test(file_name : &str) {
+    let alignment = Alignment::new("./dataset/two_sequence.maf").unwrap();
+    let graph = VariationGraph::new(&alignment);
+    let path : Vec<_> = alignment.0.iter().map(|seq| seq.name.clone()).collect();
+
+    println!("Sequences : {:?}", path);
+
+    let mut iter = path.iter();
+    while let Some(name) = iter.next() {
+        graph.print_path(name.as_bytes());
+        println!();
+    }
+    
+    /*for handle in graph.graph.handles() {
+        println!("{}", handle.id());
+        let left : Vec<_> = graph.graph.neighbors(handle, Direction::Left).map(|h| (h.id(), graph.graph.get_node_unchecked(&h.id()).sequence.as_slice())).collect();
+        let right : Vec<_> = graph.graph.neighbors(handle, Direction::Right).map(|h| (h.id(), graph.graph.get_node_unchecked(&h.id()).sequence.as_slice())).collect();
+        println!("{:?}", left);
+        println!("{:?}", right);
+    }*/
 }
 
 #[test]
 fn two_sequence() {
-    let graph = VariationGraph::new("./dataset/two_sequence.maf");
-    println!("{:#?}", graph);
-    assert_eq!(true, false);
+    run_test("./dataset/two_sequence.maf")
 }
