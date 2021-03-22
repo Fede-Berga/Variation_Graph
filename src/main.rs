273c7d284e6712f7cae1dea561152eb34334fd0d
FedericoBergamini <f.bergamini@campus.unimib.it>
2021-03-22 13:22:30 +0100

    Updated with dynamic programming

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ use variation_graph::{
 };
 
 fn main() {
-    let alignment = Alignment::new("./dataset/test_01.maf").unwrap();
+    let alignment = Alignment::new("./dataset/test_2.maf").unwrap();
 
-    let graph = VariationGraph::new(&alignment, 2 as usize);
+    let graph = VariationGraph::new(&alignment, 4 as usize);
+
+    for seq in alignment.0.iter() {
+        graph.print_path(&seq.name).unwrap();
+    }
 
     graph.print_graph();
 }
